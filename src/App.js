b36d55049b54b4e68dadbb3effab1d029a427691
Harshita <hgupta5_be18@thapar.edu>
2023-11-19 18:15:37 +0530

    app.js changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <button  className="btn btn-primary">CLick me</button>
+     <button className="btn btn-primary">CLick me</button>
     </div>
   );
 }
